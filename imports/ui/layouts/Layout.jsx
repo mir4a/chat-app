674a608cb9d7aa2282c52d125618e4fb5918a86d
Miroslav Martynov <marty.mircha@gmail.com>
2016-10-10 17:36:20 +0300

    Metatags

--- imports/ui/layouts/Layout.jsx
+++ imports/ui/layouts/Layout.jsx
@@ -3,6 +3,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 
 // React Dependencies
 import React from 'react';
+import DocumentMeta from 'react-document-meta';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
@@ -15,6 +16,17 @@ export default function Layout({ content }) {
     color: 'white',
   };
 
+  const meta = {
+    title: 'Awesome Chat',
+    description: 'Like skype but Awesome',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'react,meta,document,html,tags',
+      },
+    },
+  };
+
   const toggleLeftMenu = () => {
     $('.conversations').toggleClass('show');
   };
@@ -22,6 +34,7 @@ export default function Layout({ content }) {
   return (
     <MuiThemeProvider>
       <div>
+        <DocumentMeta {...meta} />
         <AppBar
           title={
             <a style={titleLinkStyles} href={FlowRouter.path('chats')}>Awesome Chat App</a>
